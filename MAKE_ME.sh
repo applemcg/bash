d314abdc5c19a6de82c96c08d0917ea174557648
applemcg <mcgowan@alum.mit.edu>
2018-02-18 15:56:33 -0500

    Public Library updated from Single Local directory
      This commit collects some out-of-sequence editing I did,
      still learning remote git, since I installed the Code of Conduct.
    
      A Public Library recevies functions from any number of local
      libraries, all from a single local directory.  After adding
      public_profile, the first local library in a directory to define the
      public_profile defines the base "HOME" and the public library name
      for the libraries in the local directory.
    
      To reset the HOME and library name, REMOVE the .public_profile file
      and call the public_profile function in the library initialization
      with the HOME and library name.
    
      The attempt to allow updates from any source created a messy
      sub-structure.  The pubic_update function is now greatly simplified.
      It's two sub-functions: public_{all,in}local call the various
      [lib]_public functions to collect the list of PUBLIC functions.
    
      There is no longer an attempt to selectively update only changed
      functions.
    
      Make Changes:
    
      "mk" is now "mk_dot", which merely sources all local libraries,
    
      Then, "make_loader" adds a call to "make_initialze".
    
      For our bootstrap purpose, "MAKE_ME.sh", it is important to split
      these steps. Since the two main public libs, PUBLIB and SHELF, are
      interdependent.  So, visiting the make, smpub, and shelf
      directories, first loads the libraries contents, then
      "make_initialize" each in turn, followed by the individual calls to
      public_update.
    
    big make #2, add public_profile
    
     debugging MAKE_ME, discovered need for the public_profile, such that
     all the libraries in a directory are destined for the same single
     public library
    
    boot Makefile.0
    
      adding ./MAKE_ME.sh which
      1. removes all function definitions
      2. defines local _mk function one, here, and writes on stderr:
      3.  MKs in make, shelf, smpub, then initializes in reverse order:
      4. smpub,shelf, and make, concluding with public_updates of all

---
+++ MAKE_ME.sh
@@ -0,0 +1,66 @@
+_functions () 
+{ 
+    set | awk 'NF == 2 && $2 ~ /^[(][)]$/ { print $1 }'
+}
+_mk_msg () 
+{ 
+    printf "$* ? " > /dev/tty;
+    read a < /dev/tty
+}
+_mk_one () 
+{ 
+    ( echo ONE $1 $PWD;
+    _functions | wc ) >> $err;
+    dirs;
+    _mk_msg wc -l $(wc -l $err)
+}
+_mk_funw () 
+{ 
+    grep "^$1 [(][)]" ./*lib ./*/*lib
+}
+_mk_init () 
+{ 
+    make_loader 2>> $err;
+    _mk_one INIT
+}
+_mk_here () 
+{ 
+    mk_dot 2>> $err;
+    _mk_one HERE
+}
+_acd () 
+{ 
+    cd $1;
+    $2;
+    _mk_one $2
+}
+_mk_run () 
+{ 
+    unset $( _functions| grep -v '^_' );
+    _functions;
+    dirs -c;
+    err=$PWD/make.err;
+    rm -f $err;
+    _mk_one;
+    cd ./make;
+    . mklib;
+    _acd ../make _mk_here;
+    _acd ../smpub _mk_here;
+    _acd ../shelf _mk_here;
+    _functions | grep -v '^_' | sort > ../mkfuns.txt;
+    _mk_msg all MK wc -l $(wc -l ../mkfuns.txt);
+    _acd ../shelf _mk_init;
+    _acd ../smpub _mk_init;
+    _acd ../make _mk_init;
+    _functions | grep -v '^_' | sort > ../initfuns.txt;
+    _mk_msg all INIT wc -l $(wc -l ../initfuns.txt);
+    declare -f _mk_updateall
+}
+_mk_updateall () 
+{ 
+    _acd ../shelf public_update;
+    _acd ../smpub public_update;
+    _acd ../make public_update;
+    _acd .. dirs
+}
+echo _mk_run
